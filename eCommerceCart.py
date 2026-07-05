0502eb3ce3ae72e5242aaa1505d1a92e5304a232
alexb <aborod1@wgu.edu>
2026-07-05 17:59:51 -0400

    added Cart class

--- eCommerceCart.py
+++ eCommerceCart.py
@@ -8,3 +8,8 @@ class Product:
 
     def __str__(self):
         return f"{self.name} - ${self.price:.2f}"
+
+class Cart:
+    def __init__(self):
+        self.items = []
+        self.discount = 0
