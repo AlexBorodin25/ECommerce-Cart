502441e578b177630247c159e4f1e9d397a15a0d
alexb <aborod1@wgu.edu>
2026-07-05 20:11:43 -0400

    unit test for adding product

--- test_eCommerceCart.py
+++ test_eCommerceCart.py
@@ -8,3 +8,12 @@ def test_product():
     assert product.product_id == 1
     assert product.name == "Jacket"
     assert product.price == 99.99
+
+def test_add_item():
+    cart = Cart()
+    product = Product(1, "Jacket", 99.99)
+
+    cart.add_item(product)
+
+    assert len(cart.items) == 1
+    assert cart.items[0] == product
